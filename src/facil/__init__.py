e83e22d0d5511ef9dcc047abda33b00cc8b42566
Jakob Sattler <jakob.sattler@outlook.com>
2024-07-31 09:31:48 +0200

    change structure

--- src/facil/__init__.py
+++ src/facil/__init__.py
@@ -23,6 +23,29 @@ from .networks.network import LLL_Net
 from . import utils
 from .gridsearch import GridSearch
 
+def init():
+    """ Perform all initialization steps previously done in `main_incremental.py` and stop before training.
+    """
+    global tstart
+    tstart = time.time()
+
+    load_args()
+    load_exp_name()
+    init_cuda()
+    print_args()
+    init_logger()
+    log_args()
+    utils.seed_everything()
+    init_model()
+    utils.seed_everything()
+    load_data()
+    utils.seed_everything()
+    init_approach()
+    utils.seed_everything()
+
+def train():
+    from . import training
+    return training.train()
 
 def load_base_args():
     parser = argparse.ArgumentParser(description='FACIL - Framework for Analysis of Class Incremental Learning')
@@ -216,117 +239,6 @@ def init_approach():
         appr_ft = Appr_finetuning(net, device, base_appr_kwargs, all_outputs=False)
         gridsearch = GridSearch(appr_ft, args.seed, gs_args.gridsearch_config, gs_args.gridsearch_acc_drop_thr,
                                 gs_args.gridsearch_hparam_decay, gs_args.gridsearch_max_num_searches)
-
-def init():
-    global tstart
-    tstart = time.time()
-
-    load_args()
-    load_exp_name()
-    init_cuda()
-    print_args()
-    init_logger()
-    log_args()
-    utils.seed_everything()
-    init_model()
-    utils.seed_everything()
-    load_data()
-    utils.seed_everything()
-    init_approach()
-    utils.seed_everything()
-
-
-def train():
-    print(taskcla)
-    acc_taw = np.zeros((max_task, max_task))
-    acc_tag = np.zeros((max_task, max_task))
-    forg_taw = np.zeros((max_task, max_task))
-    forg_tag = np.zeros((max_task, max_task))
-    for t, (_, ncla) in enumerate(taskcla):
-        # Early stop tasks if flag
-        if t >= max_task:
-            continue
-
-        print('*' * 108)
-        print('Task {:2d}'.format(t))
-        print('*' * 108)
-
-        # Add head for current task
-        net.add_head(taskcla[t][1])
-        net.to(device)
-
-        # GridSearch
-        if t < args.gridsearch_tasks:
-
-            # Search for best finetuning learning rate -- Maximal Plasticity Search
-            print('LR GridSearch')
-            best_ft_acc, best_ft_lr = gridsearch.search_lr(appr.model, t, trn_loader[t], val_loader[t])
-            # Apply to approach
-            appr.lr = best_ft_lr
-            gen_params = gridsearch.gs_config.get_params('general')
-            for k, v in gen_params.items():
-                if not isinstance(v, list):
-                    setattr(appr, k, v)
-
-            # Search for best forgetting/intransigence tradeoff -- Stability Decay
-            print('Trade-off GridSearch')
-            best_tradeoff, tradeoff_name = gridsearch.search_tradeoff(args.approach, appr,
-                                                                      t, trn_loader[t], val_loader[t], best_ft_acc)
-            # Apply to approach
-            if tradeoff_name is not None:
-                setattr(appr, tradeoff_name, best_tradeoff)
-
-            print('-' * 108)
-
-        # Train
-        appr.train(t, trn_loader[t], val_loader[t])
-        print('-' * 108)
-
-        # Test
-        for u in range(t + 1):
-            test_loss, acc_taw[t, u], acc_tag[t, u] = appr.eval(u, tst_loader[u])
-            if u < t:
-                forg_taw[t, u] = acc_taw[:t, u].max(0) - acc_taw[t, u]
-                forg_tag[t, u] = acc_tag[:t, u].max(0) - acc_tag[t, u]
-            print('>>> Test on task {:2d} : loss={:.3f} | TAw acc={:5.1f}%, forg={:5.1f}%'
-                  '| TAg acc={:5.1f}%, forg={:5.1f}% <<<'.format(u, test_loss,
-                                                                 100 * acc_taw[t, u], 100 * forg_taw[t, u],
-                                                                 100 * acc_tag[t, u], 100 * forg_tag[t, u]))
-            logger.log_scalar(task=t, iter=u, name='loss', group='test', value=test_loss)
-            logger.log_scalar(task=t, iter=u, name='acc_taw', group='test', value=100 * acc_taw[t, u])
-            logger.log_scalar(task=t, iter=u, name='acc_tag', group='test', value=100 * acc_tag[t, u])
-            logger.log_scalar(task=t, iter=u, name='forg_taw', group='test', value=100 * forg_taw[t, u])
-            logger.log_scalar(task=t, iter=u, name='forg_tag', group='test', value=100 * forg_tag[t, u])
-
-        # Save
-        print('Save at ' + os.path.join(args.results_path, full_exp_name))
-        logger.log_result(acc_taw, name="acc_taw", step=t)
-        logger.log_result(acc_tag, name="acc_tag", step=t)
-        logger.log_result(forg_taw, name="forg_taw", step=t)
-        logger.log_result(forg_tag, name="forg_tag", step=t)
-        logger.save_model(net.state_dict(), task=t)
-        logger.log_result(acc_taw.sum(1) / np.tril(np.ones(acc_taw.shape[0])).sum(1), name="avg_accs_taw", step=t)
-        logger.log_result(acc_tag.sum(1) / np.tril(np.ones(acc_tag.shape[0])).sum(1), name="avg_accs_tag", step=t)
-        aux = np.tril(np.repeat([[tdata[1] for tdata in taskcla[:max_task]]], max_task, axis=0))
-        logger.log_result((acc_taw * aux).sum(1) / aux.sum(1), name="wavg_accs_taw", step=t)
-        logger.log_result((acc_tag * aux).sum(1) / aux.sum(1), name="wavg_accs_tag", step=t)
-
-        # Last layer analysis
-        if args.last_layer_analysis:
-            weights, biases = last_layer_analysis(net.heads, t, taskcla, y_lim=True)
-            logger.log_figure(name='weights', iter=t, figure=weights)
-            logger.log_figure(name='bias', iter=t, figure=biases)
-
-            # Output sorted weights and biases
-            weights, biases = last_layer_analysis(net.heads, t, taskcla, y_lim=True, sort_weights=True)
-            logger.log_figure(name='weights', iter=t, figure=weights)
-            logger.log_figure(name='bias', iter=t, figure=biases)
-    # Print Summary
-    utils.print_summary(acc_taw, acc_tag, forg_taw, forg_tag)
-    print('[Elapsed time = {:.1f} h]'.format((time.time() - tstart) / (60 * 60)))
-    print('Done!')
-
-    return acc_taw, acc_tag, forg_taw, forg_tag, logger.exp_path
 
 
 def init_model():
