9a43228244edeccbc02e9126aaf5ec47a9dfc3ee
Jakob Sattler <jakob.sattler@outlook.com>
2024-08-01 13:53:10 +0200

    fix utils logger

--- src/facil/utils.py
+++ src/facil/utils.py
@@ -20,16 +20,16 @@ def seed_everything(seed=0):
 def print_summary(acc_taw, acc_tag, forg_taw, forg_tag):
     """Print summary of results"""
     for name, metric in zip(['TAw Acc', 'TAg Acc', 'TAw Forg', 'TAg Forg'], [acc_taw, acc_tag, forg_taw, forg_tag]):
-        logger.log_print('*' * 108)
-        logger.log_print(name)
+        facil.logger.log_print('*' * 108)
+        facil.logger.log_print(name)
         for i in range(metric.shape[0]):
-            logger.log_print('\t', end='')
+            facil.logger.log_print('\t', end='')
             for j in range(metric.shape[1]):
-                logger.log_print('{:5.1f}% '.format(100 * metric[i, j]), end='')
+                facil.logger.log_print('{:5.1f}% '.format(100 * metric[i, j]), end='')
             if np.trace(metric) == 0.0:
                 if i > 0:
-                    logger.log_print('\tAvg.:{:5.1f}% '.format(100 * metric[i, :i].mean()), end='')
+                    facil.logger.log_print('\tAvg.:{:5.1f}% '.format(100 * metric[i, :i].mean()), end='')
             else:
-                logger.log_print('\tAvg.:{:5.1f}% '.format(100 * metric[i, :i + 1].mean()), end='')
-            logger.log_print()
-    logger.log_print('*' * 108)
+                facil.logger.log_print('\tAvg.:{:5.1f}% '.format(100 * metric[i, :i + 1].mean()), end='')
+            facil.logger.log_print()
+    facil.logger.log_print('*' * 108)
